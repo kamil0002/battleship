9cea25871b95a1300eeea7402ac27404935707a5
kamil <kamiln022@gmail.com>
2022-02-22 23:27:12 +0100

    dragging completed but undo

--- public/assets/scripts/Board.js
+++ public/assets/scripts/Board.js
@@ -1,10 +1,13 @@
 import interact from 'interactjs';
 import Ship from './Ship';
+import { showAlert } from './utils';
 
 class Board {
   constructor(gameOptions, boardContainer) {
     this.gameOptions = gameOptions;
     this.boardContainer = boardContainer;
+
+    this._rotateShipsHandler();
   }
 
   createBoard() {
@@ -31,83 +34,129 @@ class Board {
     return boardSquares;
   }
 
-  configureDraggableShips = (boardSquares) => {
-    const shipPos = { top: 0, left: 0, right: 0, bottom: 0 };
-    const shipsHtml = Array.from(document.querySelectorAll('.ship'));
+  configureDraggableShips(boardSquares) {
+    const placedShipPos = { top: 0, left: 0, right: 0, bottom: 0 };
+    this.shipsHtml = Array.from(document.querySelectorAll('.ship'));
 
-    const ships = shipsHtml.map((ship) => new Ship(ship.dataset.name, ship.dataset.size));
-
-    const setShipPosition = (e, ship) => {
-      shipPos.top = e.target.getBoundingClientRect().top;
-      shipPos.right = e.target.getBoundingClientRect().right;
-      shipPos.left = e.target.getBoundingClientRect().left;
-      shipPos.bottom = e.target.getBoundingClientRect().bottom + ship.offsetHeight;
+    const ships = this.shipsHtml.map((ship) => new Ship(ship.dataset.name, ship.dataset.size));
+    const setplacedShipPosition = (e, ship) => {
+      placedShipPos.top = e.target.getBoundingClientRect().top;
+      placedShipPos.right = e.target.getBoundingClientRect().right;
+      placedShipPos.left = e.target.getBoundingClientRect().left;
+      placedShipPos.bottom = e.target.getBoundingClientRect().bottom;
     };
 
-    const position = { x: 0, y: 0 };
+    const isPositonTaken = (boardCells) =>
+      boardCells.some((cell) => cell.node.classList.contains('taken'));
 
-    // const Carrier = new Ship('Carrier', 5);
-
-    let shipStartNum;
-
-    // console.log(this.boardSquares);
-    // console.log(ships);
-
-    shipsHtml.forEach((ship) => {
+    let shipStartNum = 0;
+    console.log(' squares: ', boardSquares);
+    this.shipsHtml.forEach((ship) => {
+      const localShip = ship;
+      let position = { x: 0, y: 0 };
       const localShipObj = ships.find((shipEl) => shipEl.name === ship.dataset.name);
       interact(ship).draggable({
         listeners: {
           move(e) {
             position.x += e.dx;
             position.y += e.dy;
+            setplacedShipPosition(e, ship);
             e.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
-            setShipPosition(e, ship);
-
-            // tempDot.style.bottom = `${shipPos.bottom}px`;
-            // tempDot.style.left = `${shipPos.left}px`;
           },
         },
       });
-      interact(this.boardContainer)
-        .dropzone({
-          ondrop(e) {
-            // console.log(ships);
 
-            // console.log(boardSquares);
+      interact(this.boardContainer).dropzone({
+        ondrop(e) {
+          boardSquares.forEach((square, i, squaresBoard) => {
+            // console.log(placedShipPos)
 
-            boardSquares.forEach((square, i, squaresBoard) => {
+            if (!ship.classList.contains(`ship--${ship.dataset.name.toLowerCase()}--rotated`)) {
               if (
-                shipPos.top + 20 >= square.top &&
-                shipPos.top - 20 <= square.top &&
-                shipPos.left + 20 > square.left &&
-                shipPos.left - 20 < square.left &&
-                shipPos.right + 20 > squaresBoard[i + localShipObj.size - 1].right &&
-                shipPos.right - 20 < squaresBoard[i + localShipObj.size - 1].right
+                i + localShipObj.size - 1 < 100 &&
+                placedShipPos.top + 20 >= square.top &&
+                placedShipPos.top - 20 <= square.top &&
+                placedShipPos.left + 20 > square.left &&
+                placedShipPos.left - 20 < square.left &&
+                placedShipPos.right + 20 > squaresBoard[i + localShipObj.size - 1].right &&
+                placedShipPos.right - 20 < squaresBoard[i + localShipObj.size - 1].right
               ) {
                 shipStartNum = i;
               }
-            });
-
-            // console.log(boardSquares[shipStartNum]);
-            // console.log(boardSquares[shipStartNum]);
-
-            for (let i = shipStartNum; i < shipStartNum + localShipObj.size; i++) {
-              console.log(i, shipStartNum + localShipObj.size - 1);
-              if (i === shipStartNum) boardSquares[i].node.classList.add('ship-start');
-              if (i === shipStartNum + localShipObj.size - 1) {
-                boardSquares[i].node.classList.add('ship-end');
-              }
-              boardSquares[i].node.classList.add('taken');
             }
-            if (e.relatedTarget.dataset.name === localShipObj.name) ship.remove();
-          },
-        })
-        .on('dropactivate', (e) => {
-          e.target.classList.add('drop-activated');
-        });
+            if (ship.classList.contains(`ship--${ship.dataset.name.toLowerCase()}--rotated`)) {
+              console.log('Local ship: ', placedShipPos);
+              console.log('HERE: ', squaresBoard[i + localShipObj.size * 10]);
+              if (
+                i + (localShipObj.size - 1) * 10 < 100 &&
+                placedShipPos.top + 20 >= square.top &&
+                placedShipPos.top - 20 <= square.top &&
+                placedShipPos.left + 20 >= square.left &&
+                placedShipPos.left - 20 <= square.left &&
+                placedShipPos.bottom + 20 >= squaresBoard[i + (localShipObj.size - 1) * 10].bottom &&
+                placedShipPos.bottom - 20 <= squaresBoard[i + (localShipObj.size - 1) * 10].bottom
+              ) {
+                console.log('WORK');
+                shipStartNum = i;
+              }
+            }
+          });
+
+          if (e.relatedTarget.dataset.name === localShipObj.name) {
+            if (
+              isPositonTaken(boardSquares.slice(shipStartNum, shipStartNum + localShipObj.size))
+            ) {
+              localShip.style.transform = `translate(0,0)`;
+              position = { x: 0, y: 0 };
+              showAlert(
+                'You already placed some ship on this position, try a different position!',
+                3
+              );
+              return;
+            }
+            if (!ship.classList.contains(`ship--${ship.dataset.name.toLowerCase()}--rotated`)) {
+              for (let i = shipStartNum; i < shipStartNum + localShipObj.size; i++) {
+                if (i === shipStartNum) boardSquares[i].node.classList.add('ship-h-start');
+                if (i === shipStartNum + localShipObj.size - 1) {
+                  boardSquares[i].node.classList.add('ship-h-end');
+                }
+                boardSquares[i].node.classList.add('taken', 'taken--horizontal');
+              }
+            }
+
+            if (ship.classList.contains(`ship--${ship.dataset.name.toLowerCase()}--rotated`)) {
+              const squares = [boardSquares[shipStartNum]];
+              for (let i = 1; i < +ship.dataset.size; i++) {
+                squares.push(boardSquares[shipStartNum + i * 10]);
+              }
+              console.log('Jajca');
+              console.log(shipStartNum);
+              console.log(squares);
+
+              squares.forEach((square) => {
+                square.node.classList.add('taken', 'taken--vertical');
+              });
+              squares[0].node.classList.add('ship-v-start');
+              squares[squares.length - 1].node.classList.add('ship-v-end');
+            }
+
+            ship.remove();
+          }
+        },
+      });
     });
 
     return ships;
-  };
+  }
+
+  _rotateShipsHandler() {
+    const shipsContainer = document.querySelector('.ships');
+    document.querySelector('.board-btn--rotate').addEventListener('click', () => {
+      this.shipsHtml.forEach((ship) =>
+        ship.classList.toggle(`ship--${ship.dataset.name.toLowerCase()}--rotated`)
+      );
+      shipsContainer.classList.toggle('ships--rotated');
+    });
+  }
 }
 export default Board;
--- public/assets/scripts/utils.js
+++ public/assets/scripts/utils.js
@@ -1,7 +1,8 @@
-export const showModal = () => {
+export const showAlert = (msg, time = 5) => {
   const modal = document.querySelector('.modal');
+  modal.querySelector('.modal__description').textContent = msg;
   modal.classList.toggle('modal--hidden');
-  window.setTimeout(() => modal.classList.add('modal--hidden'), 4000);
+  window.setTimeout(() => modal.classList.add('modal--hidden'), time * 1000);
   modal
     .querySelector('.modal__close')
     .addEventListener('click', () => modal.classList.add('modal--hidden'));
