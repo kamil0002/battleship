d2f0ba113e139178550ab7a3ad2c18bdd929e526
kamil0002 <kamiln022@gmail.com>
2022-03-06 00:25:20 +0100

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use('/', express.static(path.join(__dirname, 'public', 'dist')));
 
 app.get('/', (req, res) => {
-  res.sendFile(`./public/dist/index.html`);
+  res.sendFile(`/public/dist/index.html`);
 });
 
 const players = [
