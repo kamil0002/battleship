47ad2a58a9d85645fcadccf022aee8c5befe9536
kamil0002 <kamiln022@gmail.com>
2022-03-06 00:27:59 +0100

    make it better

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const io = require('socket.io')(server);
 
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname, 'public', 'dist')));
-
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/assets/index.html`);
 });
